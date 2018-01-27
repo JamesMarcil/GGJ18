efb84eb61e23a98819ee0ad40a2ca8251b15cc28
James Marcil <james@jamesmarcil.com>
2018-01-27 17:36:17 -0500

    Pre-declaring event strings.

---
+++ Assets/Scripts/GameEvents.cs
@@ -0,0 +1,15 @@
+public static class GameEvents
+{
+    public const string ENABLE_COMMAND_BUTTONS = "GameEvents_ENABLE_COMMAND_BUTTONS";
+    public const string DISABLE_COMMAND_BUTTONS = "GameEvents_DISABLE_COMMAND_BUTTONS";
+
+    public const string ENABLE_START_BUTTON = "GameEvents_ENABLE_START_BUTTON";
+    public const string DISABLE_START_BUTTON = "GameEvents_DISABLE_START_BUTTON";
+
+    public const string ENQUEUE_COMMAND = "GameEvents_ENQUEUE_COMMAND";
+
+    public const string START_PROCESSING_COMMANDS = "GameEvents_START_PROCESSING_COMMANDS";
+
+    public const string GAME_SUCCESS = "GameEvents_GAME_SUCCESS";
+    public const string GAME_FAILURE = "GameEvents_GAME_FAILURE";
+}
