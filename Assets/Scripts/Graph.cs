6c7c1114b385cc654e01932b830a23e350297c75
James Marcil <james@jamesmarcil.com>
2018-01-27 11:47:34 -0500

    Update Graph internals because Unity only does 1D arrays.

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -10,22 +10,61 @@ public class Graph
     private int m_numEntries;
 
     [SerializeField]
-    private bool[,] m_adjacencyMatrix;
+    private bool[] m_adjacencyMatrix;
 
     public Graph(int numEntries)
     {
         m_numEntries = numEntries;
 
-        m_adjacencyMatrix = new bool[m_numEntries, m_numEntries];
+        m_adjacencyMatrix = new bool[m_numEntries * m_numEntries];
         for (int i = 0; i < m_numEntries; i++)
         {
-            m_adjacencyMatrix[i, i] = true;
+            int index;
+            if (GetIndexFromRowAndColumn(i, i, out index))
+            {
+                m_adjacencyMatrix[index] = true;
+            }
         }
+    }
+
+    public bool GetRowAndColumnFromIndex(int index, out int row, out int column)
+    {
+        if ((index < 0) || (index >= m_numEntries))
+        {
+            row = default(int);
+            column = default(int);
+
+            return false;
+        }
+
+        row = (index / m_numEntries);
+        column = (index % m_numEntries);
+
+        return true;
+    }
+
+    private bool GetIndexFromRowAndColumn(int row, int column, out int index)
+    {
+        if ((row < 0) || (row >= m_numEntries) || (column < 0) || (column >= m_numEntries))
+        {
+            index = default(int);
+            return false;
+        }
+
+        index = (row * m_numEntries) + column;
+
+        return true;
     }
 
     public bool IsConnected(int lhs, int rhs)
     {
-        return (m_adjacencyMatrix[lhs, rhs] || m_adjacencyMatrix[rhs, lhs]);
+        int lhsIndex;
+        int rhsIndex;
+
+        GetIndexFromRowAndColumn(lhs, rhs, out lhsIndex);
+        GetIndexFromRowAndColumn(rhs, lhs, out rhsIndex);
+
+        return (m_adjacencyMatrix[lhsIndex] || m_adjacencyMatrix[rhsIndex]);
     }
 
     public void Clear()
@@ -39,29 +78,51 @@ public class Graph
                     continue;
                 }
 
-                m_adjacencyMatrix[i, j] = false;
-                m_adjacencyMatrix[j, i] = false;
+                int lhsIndex;
+                int rhsIndex;
+
+                GetIndexFromRowAndColumn(i, j, out lhsIndex);
+                GetIndexFromRowAndColumn(j, i, out rhsIndex);
+
+                m_adjacencyMatrix[lhsIndex] = false;
+                m_adjacencyMatrix[rhsIndex] = false;
             }
         }
     }
 
     public void Connect(int lhs, int rhs)
     {
-        m_adjacencyMatrix[lhs, rhs] = true;
-        m_adjacencyMatrix[rhs, lhs] = true;
+        int lhsIndex;
+        int rhsIndex;
+
+        GetIndexFromRowAndColumn(lhs, rhs, out lhsIndex);
+        GetIndexFromRowAndColumn(rhs, lhs, out rhsIndex);
+
+        m_adjacencyMatrix[lhsIndex] = true;
+        m_adjacencyMatrix[rhsIndex] = true;
     }
 
     public void Disconnect(int lhs, int rhs)
     {
-        m_adjacencyMatrix[lhs, rhs] = false;
-        m_adjacencyMatrix[rhs, lhs] = false;
+        int lhsIndex;
+        int rhsIndex;
+
+        GetIndexFromRowAndColumn(lhs, rhs, out lhsIndex);
+        GetIndexFromRowAndColumn(rhs, lhs, out rhsIndex);
+
+        m_adjacencyMatrix[lhsIndex] = false;
+        m_adjacencyMatrix[rhsIndex] = false;
     }
 
     public IEnumerable<int> Neighbors(int id)
     {
         for (int i = 0; i < m_numEntries; i++)
         {
-            if (m_adjacencyMatrix[id, i])
+            int index;
+
+            GetIndexFromRowAndColumn(id, i, out index);
+
+            if (m_adjacencyMatrix[index])
             {
                 yield return i;
             }
