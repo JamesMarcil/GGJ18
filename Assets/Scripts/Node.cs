bdbcd3ffe87c37981082b7726074127f36cf6a94
James Marcil <james@jamesmarcil.com>
2018-01-27 15:15:35 -0500

    Adding function to query in direction.

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -12,11 +12,46 @@ public class Node : MonoBehaviour
     [HideInInspector]
     private int m_index;
 
-    private IEnumerable<GameObject> AdjacentNodes()
+    public bool GetNodeInDirection(Directions direction, out GameObject obj)
+    {
+        int row;
+        int column;
+        if (m_grid.GetRowAndColumnFromIndex(m_index, out row, out column))
+        {
+            switch (direction)
+            {
+                case Directions.NORTH:
+                    {
+                        return m_grid.GetGameObjectFromRowAndColumn(row + 1, column, out obj);
+                    }
+                case Directions.EAST:
+                    {
+                        return m_grid.GetGameObjectFromRowAndColumn(row, column + 1, out obj);
+                    }
+                case Directions.SOUTH:
+                    {
+                        return m_grid.GetGameObjectFromRowAndColumn(row - 1, column, out obj);
+                    }
+                case Directions.WEST:
+                    {
+                        return m_grid.GetGameObjectFromRowAndColumn(row, column - 1, out obj);
+                    }
+                default:
+                    {
+                        break; // No-op
+                    }
+            }
+        }
+
+        obj = default(GameObject);
+        return false;
+    }
+
+    public IEnumerable<GameObject> AdjacentNodes()
     {
         foreach (int index in m_grid.AdjacentFromIndex(m_index))
         {
-            GameObject obj; 
+            GameObject obj;
             if (m_grid.GetGameObjectFromIndex(index, out obj))
             {
                 yield return obj;
@@ -28,7 +63,7 @@ public class Node : MonoBehaviour
     {
         foreach (int index in m_grid.ConnectedNeighborsFromIndex(m_index))
         {
-            GameObject obj; 
+            GameObject obj;
             if (m_grid.GetGameObjectFromIndex(index, out obj))
             {
                 yield return obj;
